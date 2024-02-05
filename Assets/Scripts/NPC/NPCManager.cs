13fbbdefa1c6ecbae8de0760ee6e14a24a63dbb6
Dayt0w <hallman.alec13@gmail.com>
2024-02-05 11:25:25 -0500

    Necromancer finished
    Implemented the brain wash ability, currently works with any weapon, I plan to keep it that way so the future random class is viable.

--- Assets/Scripts/NPC/NPCManager.cs
+++ Assets/Scripts/NPC/NPCManager.cs
@@ -7,6 +7,7 @@ public class NPCManager : MonoBehaviour
 {
     // Start is called before the first frame update
     private Queue<Transform> enemies;
+    public float health;
     void Start()
     {
         enemies = new Queue<Transform>();
@@ -15,14 +16,18 @@ public class NPCManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       Debug.Log("enemy queue count: " + enemies.Count);
+       //Debug.Log("enemy queue count: " + enemies.Count);
     }
     public Transform GetEnemy(){
-        Transform tempTransform = enemies.Peek(); // Giving a peek of the top transform in the queue, allowing multiple allies to target the same enemy
-        if(tempTransform == null){ // if the given transform no longer exists
-            tempTransform = CleanQueue(); //Clean the queue and get a new one
+        if(enemies.Count > 0){ //If the queue has something in it.
+            Transform tempTransform = enemies.Peek(); // Giving a peek of the top transform in the queue, allowing multiple allies to target the same enemy
+            if(tempTransform == null){ // if the given transform no longer exists
+                tempTransform = CleanQueue(); //Clean the queue and get a new one
+            }
+            return tempTransform;// return the new transform
+        } else{
+            return null; // If the queue is empty, return null, the enemy will keep pinning this code until it finds an enemy. Which is bound to happen eventually.
         }
-        return tempTransform;// return the new transform
     }
     void OnTriggerStay2D(Collider2D hit){ //Every frame there's something in the queue.
         if(hit.gameObject.tag.Contains("Enemy") && !enemies.Contains(hit.gameObject.transform)){ // if the enemy doesnt contain the currently found item
